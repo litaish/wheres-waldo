489946082c9eb7cabb6f07b63ba59094caf60b06
Lita Strode <l.strode000@gmail.com>
2023-07-06 09:50:47 +0300

    Change banner text

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -5,13 +5,11 @@ const Banner = () => {
     return (
       <div className={styles.container}>
         <div className={styles.left}>
-            <h1>Find everyone!</h1>
+            <h1>Where did everyone go?</h1>
             <p>
-                The goal of the game is to find all
-                characters scattered around a picture.
-                Once you find someone - click on them!
-                Pick out a level of your choice, some
-                are harder, some are easier.
+            Join the search and uncover every hidden character! Your objective is simple: locate 
+            and click on all the characters scattered throughout the picture. Levels may vary
+            in difficulty.
             </p>
         </div>
         <img className={styles.right} src={largeLogo} alt="Wheres Waldo?" />
