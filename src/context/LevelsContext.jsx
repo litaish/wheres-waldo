dce5b80bf86e39220a513d0f45a129e9e6736679
Lita Strode <l.strode000@gmail.com>
2023-07-08 19:02:31 +0300

    Fix styling for level cards

--- src/context/LevelsContext.jsx
+++ src/context/LevelsContext.jsx
@@ -1,24 +1,12 @@
 import { createContext, useState } from "react";
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../config/firebase-config';
 
 export const LevelsContext = createContext();
 
 export const LevelsProvider = ({ children }) => {
-    const [levels, setLevels] = useState([]);
-
-    const fetchLevels = async () => {
-        await getDocs(collection(db, "levels"))
-        .then((querySnapshot)=>{               
-            const newData = querySnapshot.docs
-                .map((doc) => ({...doc.data(), id:doc.id }));
-            setLevels(newData);                
-            console.log(levels);
-        })
-      }
+    const [levels, setLevels] = useState([])
 
     return (
-       <LevelsContext.Provider value={ { levels, fetchLevels } }>
+       <LevelsContext.Provider value={ { levels, setLevels } }>
             {children}
        </LevelsContext.Provider>
     )
--- src/pages/Home/HomeView.jsx
+++ src/pages/Home/HomeView.jsx
@@ -4,9 +4,20 @@ import LevelsGrid from './LevelsGrid';
 import LevelCard from './LevelCard';
 import { useContext, useEffect } from 'react';
 import { LevelsContext } from '../../context/LevelsContext';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase-config';
 
 const HomeView = () => {
-    const { levels, fetchLevels } = useContext(LevelsContext);
+    const { levels, setLevels } = useContext(LevelsContext);
+
+    const fetchLevels = async () => {
+      await getDocs(collection(db, "levels"))
+      .then((querySnapshot)=>{               
+          const newData = querySnapshot.docs
+              .map((doc) => ({...doc.data(), id:doc.id }));
+          setLevels(newData);                
+      })
+    }
 
     // fetchLevels does not depend on any variables or props, so linter suggestion can be ignored
     useEffect(() => {
