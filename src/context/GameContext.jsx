2ccab370f409b323bfe7997d7e4babde706e853e
Lita Strode <l.strode000@gmail.com>
2023-07-12 20:19:27 +0300

    Add game data loading

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,18 +1,45 @@
 import { createContext, useState } from "react";
 import { useStopwatch } from "../hooks/useStopwatch";
+import { addDoc, collection, doc, getDoc } from 'firebase/firestore';
+import { db } from '../config/firebase-config'
 
 export const GameContext = createContext();
 
 export const GameProvider = ({ children }) => {
     const [clickCoordinates, setClickCoordinates] = useState({ x: 111, y: 222 });
-
+    const [game, setGame] = useState({});
     const { time, stop } = useStopwatch();
 
-    const handleCanvasClick = (e) => setClickCoordinates( { x: e.clientX, y: e.clientY });
+    const loadGameData = async (levelId) => {
+        // Get level data by id
+        const docRef = doc(db, 'levels', levelId);
+        const docSnap = await getDoc(docRef);
+
+        const gameObj = {
+            is_complete: false,
+            level: docSnap.data(),
+        }
+
+        if (docSnap.exists()) {
+            // return the game id
+            return addDoc(collection(db, 'game'), gameObj)
+            .then((gameRef) => {
+                setGame({ ...gameObj, id: gameRef.id });
+                return gameRef.id;
+            })
+            .catch((error) => {
+                console.log('There has been an error adding game data: ', error)
+            }); 
+        } else {
+            console.log('No level found');
+        }
+    }
+
+    const handleCanvasClick = (e) => setClickCoordinates({ x: e.clientX, y: e.clientY });
 
     return (
-       <GameContext.Provider value={ { clickCoordinates, handleCanvasClick, time, stop } }>
-        {children}
-       </GameContext.Provider>
+        <GameContext.Provider value={{ clickCoordinates, handleCanvasClick, time, stop, loadGameData, game, setGame }}>
+            {children}
+        </GameContext.Provider>
     )
 }
--- src/pages/Game/GameView.jsx
+++ src/pages/Game/GameView.jsx
@@ -2,16 +2,38 @@ import CharactersBar from './CharactersBar';
 import Canvas from './Canvas';
 import Stopwatch from './Stopwatch';
 import ScoreSubmitForm from './ScoreSubmitForm';
+import { useContext, useEffect } from 'react';
+import { GameContext } from '../../context/GameContext';
+import { useParams } from 'react-router-dom';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../config/firebase-config'
 
 const GameView = () => {
-    return (
-      <main>
-        <ScoreSubmitForm />
-        <CharactersBar />
-        <Stopwatch />
-        <Canvas />
-      </main>
-    );
-  }
-  
-  export default GameView;
+  const { loadGameData, setGame } = useContext(GameContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    let unsubscribeFromSnapshot;
+
+    const fetchGameDataAndSubscribe = async () => {
+      const gameId = await loadGameData(id);
+      unsubscribeFromSnapshot = onSnapshot(doc(db, 'game', gameId), (doc) => setGame({ ...doc.data(), id: doc.id}));
+    }
+
+    fetchGameDataAndSubscribe();
+
+    // Unsubscribe from document on unmount
+    return unsubscribeFromSnapshot;
+  }, [])
+
+  return (
+    <main>
+      <ScoreSubmitForm />
+      <CharactersBar />
+      <Stopwatch />
+      <Canvas />
+    </main>
+  );
+}
+
+export default GameView;
